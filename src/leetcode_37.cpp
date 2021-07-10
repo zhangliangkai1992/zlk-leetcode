3aa3a9d1cc9b15c60f7027cbf51c1275d3812afe
LiangKai Zhang <zhangliangkai.wh@gmail.com>
2021-07-10 16:33:16 +0800

    leetcode 37 update

--- src/leetcode_37.cpp
+++ src/leetcode_37.cpp
@@ -7,80 +7,102 @@
 
 // hard
 class Solution {
-  inline int GetXByIndex(int index) { return index / 9; }
-  inline int GetYByIndex(int index) { return index % 9; }
-  int FindNext(const std::vector<std::vector<char>>& board, int index) {
-    for (int i = index; i < 81; ++i) {
-      if (board[GetXByIndex(i)][GetYByIndex(i)] == '.') return i;
+  const int BLOCK = 3;
+  const int SZ = BLOCK * BLOCK;
+  const int BOUND = SZ * SZ;
+  const char ANY = '.';
+  const int ONE = '1';
+
+  inline int GetX(int index) { return index / SZ; }
+
+  inline int GetY(int index) { return index % SZ; }
+
+  int FindNext(const std::vector<std::vector<char>>& board, int start) {
+    for (int i = start; i < BOUND; ++i) {
+      if (board[GetX(i)][GetY(i)] == ANY) return i;
     }
-    return 81;
+    return BOUND;
   }
 
-  bool check(const std::vector<std::vector<char>>& board, int pos, char value) {
-    int x = GetXByIndex(pos);
-    int y = GetYByIndex(pos);
-    for (int i = 0; i < 9; ++i) {
-      if (i != x && board[i][y] == value) return false;
-      if (i != y && board[x][i] == value) return false;
-    }
-    int lowX = x / 3 * 3;
-    int lowY = y / 3 * 3;
-    for (int i = lowX; i < lowX + 3; ++i) {
-      for (int j = lowY; j < lowY + 3; ++j) {
-        if (board[i][j] == value) return false;
-      }
-    }
-    return true;
-  }
-
-  // 返回从index位置是否能解出答案
-  bool dfs(std::vector<std::vector<char>>* pBoard, int index) {
+  bool Dfs(std::vector<std::vector<char>>* pBoard, int start) {
     auto& board = *pBoard;
-    int next = FindNext(board, index);
-    if (next >= 81) return true;
-    for (int i = 1; i <= 9; ++i) {
-      char value = '0' + i;
-      if (check(board, next, value)) {
-        board[GetXByIndex(next)][GetYByIndex(next)] = value;
-        if (dfs(pBoard, next)) return true;
-        board[GetXByIndex(next)][GetYByIndex(next)] = '.';
+    int next = FindNext(board, start);
+    if (next >= BOUND) return true;
+    std::vector<bool> exist(SZ, false);
+    auto x = GetX(next);
+    auto y = GetY(next);
+    auto row = x / 3 * 3;
+    auto col = y / 3 * 3;
+    for (int i = 0; i < SZ; ++i) {
+      if (board[x][i] != ANY) {
+        int index = board[x][i] - ONE;
+        exist[index] = true;
+      }
+      if (board[i][y] != ANY) {
+        int index = board[i][y] - ONE;
+        exist[index] = true;
+      }
+      auto p = row + i / BLOCK;
+      auto q = col + i % BLOCK;
+      if (board[p][q] != ANY) {
+        int index = board[p][q] - ONE;
+        exist[index] = true;
       }
     }
+    for (int i = 0; i < SZ; ++i) {
+      if (!exist[i]) {
+        board[x][y] = static_cast<char>(i + ONE);
+        if (Dfs(pBoard, next + 1)) return true;
+      }
+    }
+    board[x][y] = ANY;
     return false;
   }
 
  public:
   void solveSudoku(std::vector<std::vector<char>>* pBoard) {
     auto& board = *pBoard;
-    dfs(pBoard, 0);
+    Dfs(&board, 0);
   }
 };
 
-TEST(leetcode_37, 1) {
-  std::vector<std::vector<char>> board = {
-      {'5', '3', '.', '.', '7', '.', '.', '.', '.'},
-      {'6', '.', '.', '1', '9', '5', '.', '.', '.'},
-      {'.', '9', '8', '.', '.', '.', '.', '6', '.'},
-      {'8', '.', '.', '.', '6', '.', '.', '.', '3'},
-      {'4', '.', '.', '8', '.', '3', '.', '.', '1'},
-      {'7', '.', '.', '.', '2', '.', '.', '.', '6'},
-      {'.', '6', '.', '.', '.', '.', '2', '8', '.'},
-      {'.', '.', '.', '4', '1', '9', '.', '.', '5'},
-      {'.', '.', '.', '.', '8', '.', '.', '7', '9'}};
-  decltype(board) expect = {{'5', '3', '4', '6', '7', '8', '9', '1', '2'},
-                            {'6', '7', '2', '1', '9', '5', '3', '4', '8'},
-                            {'1', '9', '8', '3', '4', '2', '5', '6', '7'},
-                            {'8', '5', '9', '7', '6', '1', '4', '2', '3'},
-                            {'4', '2', '6', '8', '5', '3', '7', '9', '1'},
-                            {'7', '1', '3', '9', '2', '4', '8', '5', '6'},
-                            {'9', '6', '1', '5', '3', '7', '2', '8', '4'},
-                            {'2', '8', '7', '4', '1', '9', '6', '3', '5'},
-                            {'3', '4', '5', '2', '8', '6', '1', '7', '9'}};
-  Solution s;
-  s.solveSudoku(&board);
-  for (int i = 0; i < 9; ++i) {
-    for (int j = 0; j < 9; ++j) {
-      EXPECT_EQ(board[i][j], expect[i][j]);
+std::vector<std::vector<char>> convert(const std::vector<std::string>& input) {
+  std::vector<std::vector<char>> res;
+  for (auto&& row : input) {
+    std::vector<char> vec;
+    for (auto&& c : row) {
+      vec.push_back(c);
     }
+    res.push_back(vec);
+  }
+  return res;
+}
+std::vector<std::string> convert(const std::vector<std::vector<char>>& input) {
+  std::vector<std::string> res;
+  for (auto&& row : input) {
+    std::string str;
+    for (auto&& c : row) {
+      str += c;
+    }
+    res.push_back(str);
+  }
+  return res;
+}
+
+TEST(leetcode_37, 1) {
+  std::vector<std::string> board = {"53..7....", "6..195...", ".98....6.",
+                                    "8...6...3", "4..8.3..1", "7...2...6",
+                                    ".6....28.", "...419..5", "....8..79"};
+  decltype(board) expect = {"534678912", "672195348", "198342567",
+                            "859761423", "426853791", "713924856",
+                            "961537284", "287419635", "345286179"};
+  auto input = convert(board);
+  Solution s;
+  s.solveSudoku(&input);
+  std::cout << std::endl << input[0][2] << std::endl;
+  auto out = convert(input);
+  for (int i = 0; i < 9; ++i) {
+    // std::cout << out[i] << std::endl;
+    EXPECT_EQ(out[i], expect[i]);
   }
 }
